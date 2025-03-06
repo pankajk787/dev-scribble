6a2efb5968a6371f805a734b6a681591a29d8bfe
Pankaj <pankajkumarhzb787@gmail.com>
2025-03-07 00:47:22 +0530

    UI fixes

--- src/containers/canavas-free-draw/index.jsx
+++ src/containers/canavas-free-draw/index.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
-import './style.css';
+import React, { useState } from "react";
+import "./style.css";
 
 const CanvasFreeDraw = (props) => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const handleToggleFreeCanvas = () => {
-        setOpen(prev => !prev);
-    }
+  const handleToggleFreeCanvas = () => {
+    setOpen((prev) => !prev);
+  };
 
   return (
-    <div className={`canvasFreeDrawOverLay ${open ? "open" : ""}`}>
+    <>
+      <div className={`canvasFreeDrawOverLay ${open ? "open" : ""}`}>
         <div className={`canvasFreeDrawContainer ${open ? "open" : ""}`}>
-        <div className='canvasFreeDraw'>
-            CanvasFreeDraw
+          <div className="canvasFreeDraw">CanvasFreeDraw</div>
         </div>
-        <button className={`scrollTool ${open ? "open" : ""}`} onClick={handleToggleFreeCanvas}>
-            ✒️
-        </button>
-        </div>
-    </div>
-  )
-}
+      </div>
+      <button
+        className={`scrollTool ${open ? "open" : ""}`}
+        onClick={handleToggleFreeCanvas}
+      >
+        ✒️
+      </button>
+    </>
+  );
+};
 
 export default CanvasFreeDraw;
