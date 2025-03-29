e088d0b3896a6dbb246e0a3d7d144ebc93ec6f1c
Roshan Kumar <rsahu7989@gmail.com>
2025-03-30 02:31:31 +0530

    add message action constants for sending and receiving messages

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -8,6 +8,8 @@ const ACTIONS = {
     SYNC_CODE: 'sync-code',
     SYNC_CANVAS_CONTENT: 'sync-canvas-content',
     CANVAS_CHANGE: 'canvas-update',
+    SEND_MESSAGE: "send-message",
+    RECIEVED_MESSAGE: 'recieved_message'
 }
 
 module.exports = ACTIONS;
